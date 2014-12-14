cd8adab7a22560557843f0a29b8267ac363c7588
richelbilderbeek <richel@richelbilderbeek.nl>
2014-12-14 17:40:41 +0100

    Creating TeX article

---
+++ create_tex_article.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+maxima_input_file="create_tex_article.txt"
+tex_output_file="create_tex_article_output.tex"
+
+if [ -e $tex_output_file ]
+then
+  rm $tex_output_file
+fi
+
+maxima -b $maxima_input_file
+pdflatex $tex_output_file
